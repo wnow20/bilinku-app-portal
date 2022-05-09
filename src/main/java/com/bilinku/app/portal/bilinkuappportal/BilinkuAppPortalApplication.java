e329141af299458e8cf518dd9ea2500c10ae96f6
wnow20 <tianshi_0156@126.com>
2022-05-09 23:15:11 +0800

    first commit

---
+++ src/main/java/com/bilinku/app/portal/bilinkuappportal/BilinkuAppPortalApplication.java
@@ -0,0 +1,13 @@
+package com.bilinku.app.portal.bilinkuappportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilinkuAppPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilinkuAppPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bilinku/app/portal/bilinkuappportal/BilinkuAppPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilinku.app.portal.bilinkuappportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilinkuAppPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
